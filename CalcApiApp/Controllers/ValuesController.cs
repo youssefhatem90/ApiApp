413711be314b8d3f169eac79b55e5f441da4c317
youssefhatem90 <youssefhatem@live.com>
2024-03-28 23:14:46 +0000

    first commit

---
+++ CalcApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleCalcClassLibrary;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CalcApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        Calc myCalc = new Calc();
+
+        // GET: api/<ValuesController>
+        [HttpGet("/average")]
+        public int Average([FromQuery]int[]nums)
+        {
+            return myCalc.Average(nums);
+        }
+
+        [HttpGet("/smallest")]
+        public int Smallest([FromQuery]int[]nums)
+        {
+            return myCalc.Smallest(nums);
+        }
+
+        [HttpGet("/largest")]
+        public int Largest([FromQuery]int[]nums)
+        {
+            return myCalc.Largest(nums);
+        }
+    }
+}
